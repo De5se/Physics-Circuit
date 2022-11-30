a000aa42b0484d46eea2af7f1cb7efd334d07c43
Vseslav <Vseslav2244@gmail.com>
2022-12-01 00:48:53 +0300

    Level Manager

---
+++ Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace _Scripts
+{
+    public class LevelManager : MonoBehaviour
+    {
+        [SerializeField] private Button infoButton;
+        [SerializeField] private Button reloadSceneButton;
+        [SerializeField] private Button settingsButton;
+
+
+        private void Start()
+        {
+            infoButton.onClick.RemoveAllListeners();
+            infoButton.onClick.AddListener(InfoButtonListener);
+            reloadSceneButton.onClick.RemoveAllListeners();
+            reloadSceneButton.onClick.AddListener(ReloadButtonListener);
+            settingsButton.onClick.RemoveAllListeners();
+            settingsButton.onClick.AddListener(SettingsButtonListener);
+        }
+
+
+        private void InfoButtonListener()
+        {
+            throw new NotImplementedException();
+        }
+        
+        private void ReloadButtonListener()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        private void SettingsButtonListener()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
