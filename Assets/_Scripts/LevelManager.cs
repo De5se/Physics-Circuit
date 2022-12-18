853a5b6644ab07e9f6af6236f68d5bc7d33ea0c7
Vseslav <Vseslav2244@gmail.com>
2022-12-18 15:08:02 +0300

    Fonts update

--- Assets/_Scripts/LevelManager.cs
+++ Assets/_Scripts/LevelManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -7,35 +6,17 @@ namespace _Scripts
 {
     public class LevelManager : MonoBehaviour
     {
-        [SerializeField] private Button infoButton;
         [SerializeField] private Button reloadSceneButton;
-        [SerializeField] private Button settingsButton;
-
 
         private void Start()
         {
-            infoButton.onClick.RemoveAllListeners();
-            infoButton.onClick.AddListener(InfoButtonListener);
             reloadSceneButton.onClick.RemoveAllListeners();
             reloadSceneButton.onClick.AddListener(ReloadButtonListener);
-            settingsButton.onClick.RemoveAllListeners();
-            settingsButton.onClick.AddListener(SettingsButtonListener);
-        }
-
-
-        private void InfoButtonListener()
-        {
-            throw new NotImplementedException();
         }
         
         private void ReloadButtonListener()
         {
             SceneManager.LoadScene(0);
-        }
-
-        private void SettingsButtonListener()
-        {
-            throw new NotImplementedException();
         }
     }
 }
